d18f4483f456f611b6ffd4c34e82955f4a5b3fd8
Matthew DeVenny <matt@boxboat.com>
2021-05-27 14:43:09 -0700

    Generate self signed certs for webhook
    Signed-off-by: Matthew DeVenny <matt@boxboat.com>

--- cmd/server.go
+++ cmd/server.go
@@ -20,43 +20,133 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	dockhand "github.com/boxboat/dockhand-secrets-operator/pkg/apis/dockhand.boxboat.io/v1alpha1"
 	"github.com/boxboat/dockhand-secrets-operator/pkg/common"
 	"github.com/boxboat/dockhand-secrets-operator/pkg/k8s"
 	"github.com/boxboat/dockhand-secrets-operator/pkg/webhook"
 	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/client-go/tools/leaderelection"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 type ServerArgs struct {
-	serverPort        int
-	serverCert        string
-	serverKey         string
-	serviceName       string
-	serviceNamespace  string
-	selfSignCerts     bool
+	serverPort       int
+	serverCert       string
+	serverKey        string
+	serviceName      string
+	serviceId        string
+	serviceNamespace string
+	selfSignCerts    bool
 }
 
 var (
 	serverArgs ServerArgs
 )
 
-func runServer(ctx context.Context) {
+func certManager() {
+	lock, err := k8s.GetLeaseLock(serverArgs.serviceId, serverArgs.serviceName, serverArgs.serviceNamespace)
+	common.ExitIfError(err)
 
+	leaderCtx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	leaderelection.RunOrDie(leaderCtx, leaderelection.LeaderElectionConfig{
+		Lock:          lock,
+		LeaseDuration: 15 * time.Second,
+		RenewDeadline: 10 * time.Second,
+		RetryPeriod:   2 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				var cert *tls.Certificate
+				checkTime := time.Now()
+				for {
+					// only retrieve the certificate at startup and then once a day after that
+					if cert == nil || time.Now().Add(time.Hour*-24).After(checkTime) {
+						common.Log.Infof("checking certificate %s/%s", serverArgs.serviceNamespace, serverArgs.serviceName)
+						checkTime = time.Now()
+						cert, err = k8s.GetServiceCertificate(leaderCtx, serverArgs.serviceName, serverArgs.serviceNamespace)
+						if err != nil && !errors.IsNotFound(err) {
+							common.ExitIfError(err)
+						}
+					}
+					if cert == nil || common.ValidDaysRemaining(cert.Certificate[0]) < 30 {
+						common.Log.Infof("Renewing self signed certificate")
+						caPem, caKey, err := common.GenerateSelfSignedCA(serverArgs.serviceName + "-ca")
+						common.ExitIfError(err)
+						err = k8s.UpdateCABundleForWebhook(leaderCtx, serverArgs.serviceName+".dockhand.boxboat.io", caPem)
+						common.ExitIfError(err)
+						dnsNames := []string{
+							serverArgs.serviceName + "." + serverArgs.serviceNamespace,
+							serverArgs.serviceName + "." + serverArgs.serviceNamespace + ".svc"}
+
+						cert, key, err := common.GenerateSignedCert(serverArgs.serviceName, dnsNames, caPem, caKey)
+						common.ExitIfError(err)
+						err = k8s.UpdateTlsCertificateSecret(leaderCtx, serverArgs.serviceName, serverArgs.serviceNamespace, cert, key, caPem)
+						common.ExitIfError(err)
+
+						webhook, err := k8s.GetDeployment(leaderCtx, serverArgs.serviceName, serverArgs.serviceNamespace)
+						common.ExitIfError(err)
+						checksum, err := k8s.GetSecretsChecksum(leaderCtx, []string{serverArgs.serviceName}, serverArgs.serviceNamespace)
+						common.ExitIfError(err)
+						if webhook.Spec.Template.Annotations == nil {
+							webhook.Spec.Template.Annotations = make(map[string]string)
+						}
+						webhook.Spec.Template.Annotations[dockhand.SecretChecksumAnnotationKey] = checksum
+						webhook, err = k8s.UpdateDeployment(leaderCtx, webhook, serverArgs.serviceNamespace)
+						if err != nil {
+							common.Log.Warnf("Could not update deployment %v", err)
+						}
+					}
+					time.Sleep(time.Second * 5)
+				}
+			},
+			OnStoppedLeading: func() {
+				common.Log.Infof("No longer leading")
+			},
+			OnNewLeader: func(identity string) {
+				if identity == serverArgs.serviceId {
+					return
+				}
+			},
+		},
+		WatchDog:        nil,
+		ReleaseOnCancel: true,
+		Name:            serverArgs.serviceId,
+	})
+
+}
+
+func runServer(ctx context.Context) {
 	var err error
 	tlsPair := tls.Certificate{}
 	if serverArgs.selfSignCerts {
-		tlsPair, err = k8s.GetServiceCertificate(ctx, serverArgs.serviceName, serverArgs.serviceNamespace)
-		common.ExitIfError(err)
-		if err := k8s.UpdateCABundleForWebhook(ctx, serverArgs.serviceName + ".dockhand.boxboat.io", serverArgs.serviceNamespace); err != nil {
-			common.ExitIfError(err)
-		}
+		go certManager()
 	} else {
 		tlsPair, err = tls.LoadX509KeyPair(serverArgs.serverCert, serverArgs.serverKey)
 		common.ExitIfError(err)
 	}
+
+	attempt := 0
+	for {
+		if attempt < 10 {
+			cert, err := k8s.GetServiceCertificate(ctx, serverArgs.serviceName, serverArgs.serviceNamespace)
+			if errors.IsNotFound(err) {
+				time.Sleep(5 * time.Second)
+			} else if err != nil {
+				common.Log.Warnf("error retrieving certificate, %v", err)
+			} else {
+				tlsPair = *cert
+				break
+			}
+			attempt += 1
+		}
+	}
+
+	common.Log.Infof("Starting server")
 
 	server := &webhook.Server{
 		Server: &http.Server{
@@ -131,6 +221,12 @@ func init() {
 		"key",
 		"/tls/server.key",
 		"x509 server certificate")
+
+	startServerCmd.Flags().StringVar(
+		&serverArgs.serviceId,
+		"webhook-id",
+		"",
+		"webhook server id")
 
 	startServerCmd.Flags().BoolVar(
 		&serverArgs.selfSignCerts,
